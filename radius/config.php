35687b8ad36b534fbd23932b45af4814a0b4ea61
bsbudhwar <birendersinghbudhwar@outlook.com>
2020-06-16 21:22:37 +0530

    Create config.php

---
+++ radius/config.php
@@ -0,0 +1,18 @@
+<?php
+// General
+$general_timezone = 'Asia/Kolkata';
+
+// FreeRADIUS database options
+$freeradius_db_host = 'localhost';
+$freeradius_db_username = 'radius';
+$freeradius_db_password = 'xxxxxx';
+$freeradius_db_database = 'radius';
+
+// WHMCS API
+$whmcs_api_url = 'https://whmcs.yourdomain.com/';
+$whmcs_api_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx';
+$whmcs_api_identifier = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx';
+
+// Paths to radclient
+$radclient_path = '/usr/bin/radclient';
+?>

f1efb80dbf4eee677e66196fc3626336e120ea0a
bsbudhwar <birendersinghbudhwar@outlook.com>
2020-06-16 21:18:43 +0530

    Create freeradius.php

---
+++ modules/servers/freeradius/freeradius.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Freeradius Server Module
+ * @copyright Copyright (c) BSB DATA AND INFORMATION PROCESSING SERVICES PRIVATE LIMITED
+ * @auther Birender Singh Budhwar
+ */
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}

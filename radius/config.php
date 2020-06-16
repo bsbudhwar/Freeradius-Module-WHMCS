573d103795b8f796f58ca97e6074de21099fede2
bsbudhwar <birendersinghbudhwar@outlook.com>
2020-06-16 21:29:47 +0530

    Update config.php

--- radius/config.php
+++ radius/config.php
@@ -8,11 +8,4 @@ $freeradius_db_username = 'radius';
 $freeradius_db_password = 'xxxxxx';
 $freeradius_db_database = 'radius';
 
-// WHMCS API
-$whmcs_api_url = 'https://whmcs.yourdomain.com/';
-$whmcs_api_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx';
-$whmcs_api_identifier = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx';
-
-// Paths to radclient
-$radclient_path = '/usr/bin/radclient';
-?>
+// WHMCS API & radclient path etc...

9272d0127b0c5632206010e179eb9acf8939ef2e
bsbudhwar <birendersinghbudhwar@outlook.com>
2023-04-01 23:48:27 +0530

    Update freeradius.php

--- modules/servers/freeradius/freeradius.php
+++ modules/servers/freeradius/freeradius.php
@@ -1,11 +1,32 @@
 <?php
+
 /**
- * Freeradius Server Module
- * @copyright Copyright (c) BSB DATA AND INFORMATION PROCESSING SERVICES PRIVATE LIMITED
- * @auther Birender Singh Budhwar
- */
-if (!defined("WHMCS")) {
-    die("This file cannot be accessed directly");
+ *
+ * WHMCS Freeradius Server Module
+ * Version	  : 2.0.2
+ * Author     : Birender Singh Budhwar
+ * Release on : 01.04.2023
+ * Website    : https://www.bsbdips.com
+ *
+ **/
+
+use WHMCS\Database\Capsule;
+
+// Define product configuration options.
+function freeradius_ConfigOptions()
+{
+    $configarray = array(
+     "Group Name" => array( "Type" => "text", "Size" => "25", "Default" => "", "Description" => "Enter Data Plan Name"),
+	 "Usage Limit" => array( "Type" => "text", "Size" => "25", "Default" => "", "Description" => "Data limit in bytes. Use 0 or leave blank to disable"),
+	 "Rate Limit" => array( "Type" => "text", "Size" => "25", "Default" => "", "Description" => "Bandwidth Rate limit. Use 10M/10M to setup download and upload limit."),
+	 "Session Limit" => array( "Type" => "text", "Size" => "25", "Default" => "", "Description" => "Set user session limit, Use 0 or leave blank to disable"),
+    );
+    return $configarray;
 }
 
-Then PHP Code for futher module actions.....
+// Define user manual or download link to any file
+function freeradius_download($params) {
+   header( 'Location: https://support.bsbdips.com' ) ;
+}
+
+Further code starts here...
